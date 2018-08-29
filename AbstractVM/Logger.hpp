6fd0acd5d176c0261bddd47834fc01c2a46dbfdb
Andrii Radiuk <andrii.radiuk@harmonicinc.com>
2018-08-29 16:41:35 +0300

    Delete unneeded files

--- AbstractVM/Logger.hpp
+++
@@ -1,3 +0,0 @@
-#include <iostream>
-
-void Logger(std::string str, std::string str1="", std::string str2="",  std::string str3="",  std::string str4="",  std::string str5="",  std::string str6="",  std::string str7="",  std::string str8="",  std::string str9="",  std::string str10="");
